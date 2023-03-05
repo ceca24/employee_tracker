0b5ec1c98803c5c92b3eb2a14bae566ade2d86cf
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 23:35:15 -0600

    added code to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
