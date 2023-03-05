98e3b786beaa0281a12496f8ed1df1734d340bcb
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 23:38:37 -0600

    added code to create tables for roles and employees in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,3 +8,24 @@ CREATE TABLE departments (
   name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES departments(id)
+
+  
+);
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager INT,
+  FOREIGN KEY (manager)
+  REFERENCES employees(id),
+  FOREIGN KEY (role_id)
+  REFERENCES roles(role_id)
+);
