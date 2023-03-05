1b0427be916a95fe9ba7d121ee99d5817428262a
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 23:48:58 -0600

    added code for roles in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,12 @@ VALUES ("Human Resources"),
        ("IT"),
        ("Marketing");
 
+INSERT INTO roles (title, department_id, salary)
+VALUES ("Talent Management", 1, 115000),
+       ("Manager", 4, 110000),
+       ("Executive Assistant", 3, 75000),
+       ("Data Analyst", 4, 90000),
+       ("Accountant", 2, 225000),
+       ("Trainer", 5, 125000),
+       ("Attorney", 3, 350000);
+
