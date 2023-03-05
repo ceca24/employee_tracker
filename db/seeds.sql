70f36434dd24bbbbadbbfb44f5174272d8c70846
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-04 23:53:49 -0600

    added code for employees in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,3 +14,11 @@ VALUES ("Talent Management", 1, 115000),
        ("Trainer", 5, 125000),
        ("Attorney", 3, 350000);
 
+INSERT INTO employees (first_name, last_name, manager, role_id)
+VALUES ("Erin", "Creet", NULL, 11),
+       ("Zeke", "Jones", 1, 42),
+       ("James", "Kreeger", NULL, 23),
+       ("Ayalah", "Ora", 2, 4),
+       ("Tom", "Arro", 2, 55),
+       ("Jonny", "Fredrickson",NULL, 76),
+       ("Sean", "Higgs", 3, 77);
